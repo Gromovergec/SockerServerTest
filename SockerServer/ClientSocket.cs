40af722a85909d6ad649e944c0906337ab96533b
Admin <Admin@DESKTOP-AB56RSL>
2021-11-29 22:22:55 +0200

    Add project files.

---
+++ SockerServer/ClientSocket.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SockerServer
+{
+    internal class ClientSocket
+    {
+        private TcpClient _tcpClient;
+        private ConcurrentDictionary<IPAddress, int> _connectionDic;
+        private byte[] buffer = new byte[128];
+        public ClientSocket(TcpClient tcpClient, ConcurrentDictionary<IPAddress, int> connectionDic)
+        {
+            _tcpClient = tcpClient;
+            _connectionDic = connectionDic;
+        }
+
+        public void Handler()
+        {
+            var num = 0;
+            NetworkStream networkStream = null;
+            try
+            {
+                var greeating = @"Hi user, enter a number for get the sum all entered number";
+                buffer = Encoding.UTF8.GetBytes(greeating);
+                networkStream = _tcpClient.GetStream();
+                networkStream.Write(buffer, 0, buffer.Length);
+
+                while (true)
+                {
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = networkStream.Read(buffer, 0, buffer.Length);
+                        var recive = Encoding.UTF8.GetString(buffer, 0, bytes);
+                        if (int.TryParse(recive, out num))
+                        {
+
+                        }
+
+
+                    } while (networkStream.DataAvailable);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                if (networkStream != null)
+                {
+                    networkStream.Close();
+                }
+                if (_tcpClient != null)
+                {
+                    _tcpClient.Close();
+                }
+            }
+        }
+
+    }
+}
---
+++ SockerServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SockerServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
